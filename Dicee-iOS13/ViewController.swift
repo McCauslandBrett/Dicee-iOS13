ca1cf533dadc73addd4775b85b48afff862227fd
Brett McCausland <brettmccausland@gmail.com>
2022-02-06 17:07:49 -0600

    Example project 2 Start

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -9,10 +9,13 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var diceImageViewOne: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        diceImageViewOne.image = #imageLiteral(resourceName: "DiceSix")
+        
+        
     }
 
 
